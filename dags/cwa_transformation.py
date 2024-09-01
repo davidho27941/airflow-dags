73fab702f355b45109a75846926925e905d52768
david27941 <davidho.prime@gmail.com>
2024-09-01 03:26:24 +0000

    feat: create intermediate schema if not exists.

--- dags/cwa_transformation.py
+++ dags/cwa_transformation.py
@@ -64,6 +64,7 @@ def snowflake_preflight_check(ti, **context):
         
         cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {snowflake_schema_cwb};")
         cursor.execute(f"CREATE SCHEMA IF NOT EXISTS intermediate_dev;")
+        cursor.execute(f"CREATE SCHEMA IF NOT EXISTS transformerd_dev;")
         
         cursor.execute(f"CREATE TABLE {snowflake_schema_cwb}.raw IF NOT EXISTS (filename string, raw_data VARIANT);")
         
