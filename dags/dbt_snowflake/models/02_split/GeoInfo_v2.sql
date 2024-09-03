38cc0b745c7be2d6c88857067579e8e666f657d4
david27941 <davidho.prime@gmail.com>
2024-09-03 05:16:33 +0000

    feat: update cte.

--- dags/dbt_snowflake/models/02_split/GeoInfo_v2.sql
+++ dags/dbt_snowflake/models/02_split/GeoInfo_v2.sql
@@ -1,23 +1,27 @@
 WITH source_data AS (
     SELECT * FROM {{ this }}
+), new_data AS(
+    SELECT 
+        first_layer.value:status::varchar AS status,
+        first_layer.value:StationID::varchar AS StationID,
+        first_layer.value:StationName::varchar AS StationName,
+        first_layer.value:StationNameEN::varchar AS StationNameEN,
+        first_layer.value:StationAltitude::float AS StationAltitude,
+        first_layer.value:StationLongitude::float AS StationLongitude,
+        first_layer.value:CountyName::varchar AS CountyName,
+        first_layer.value:Location::varchar AS Location,
+        first_layer.value:StationStartDate::varchar AS StationStartDate,
+        first_layer.value:StationEndDate::varchar AS StationEndDate,
+        first_layer.value:Notes::varchar AS Notes,
+        first_layer.value:OriginalStationID::varchar AS OriginalStationID,
+        first_layer.value:NewStationID::varchar AS NewStationID,
+    FROM {{ source('cwb_raw_json_stn', 'raw_stn')}} AS New_data,
+        LATERAL FLATTEN(input => RAW_DATA:records:data:stationStatus:station) first_layer;
 )
 
-SELECT 
-    first_layer.value:status::varchar AS status,
-    first_layer.value:StationID::varchar AS StationID,
-    first_layer.value:StationName::varchar AS StationName,
-    first_layer.value:StationNameEN::varchar AS StationNameEN,
-    first_layer.value:StationAltitude::float AS StationAltitude,
-    first_layer.value:StationLongitude::float AS StationLongitude,
-    first_layer.value:CountyName::varchar AS CountyName,
-    first_layer.value:Location::varchar AS Location,
-    first_layer.value:StationStartDate::varchar AS StationStartDate,
-    first_layer.value:StationEndDate::varchar AS StationEndDate,
-    first_layer.value:Notes::varchar AS Notes,
-    first_layer.value:OriginalStationID::varchar AS OriginalStationID,
-    first_layer.value:NewStationID::varchar AS NewStationID,
-FROM {{ source('cwb_raw_json_stn', 'raw_stn')}} AS New_data,
-    LATERAL FLATTEN(input => RAW_DATA:records:data:stationStatus:station) first_layer;
+SELECT
+    *
+FROM new_data
 {% if is_incremental() %}
     WHERE 
         New_data.StationID NOT IN (
