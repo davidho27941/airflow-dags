00efe2abedad3e687974e92c036bcabdd390ecc3
david27941 <davidho.prime@gmail.com>
2024-09-03 05:32:43 +0000

    feat: use cte for incremental

--- dags/dbt_snowflake/models/03_transform/Weather_Records_v2.sql
+++ dags/dbt_snowflake/models/03_transform/Weather_Records_v2.sql
@@ -1,18 +1,29 @@
+WITH old_data AS (
+    SELECT * FROM {{ this }}
+), new_data AS (
+    SELECT
+        DISTINCT
+            StationId,
+            StationName,
+            ObsTime,
+            Weather,
+            AirTemperature,
+            AirPressure,
+            RelativeHumidity,
+            WindSpeed,
+            WindDirection,
+            WindDirectionGust,
+            PeakGustSpeed,
+            Precipitation,
+            SunshineDuration_10Min,
+            Visibility,
+            UVIndex,
+    FROM {{ ref('extracted_json_v2') }}
+)
+
 SELECT
-    DISTINCT
-        StationId,
-        StationName,
-        ObsTime,
-        Weather,
-        AirTemperature,
-        AirPressure,
-        RelativeHumidity,
-        WindSpeed,
-        WindDirection,
-        WindDirectionGust,
-        PeakGustSpeed,
-        Precipitation,
-        SunshineDuration_10Min,
-        Visibility,
-        UVIndex,
-FROM {{ ref('extracted_json_v2') }}
+    *
+FROM new_data
+{% if is_incremental()%}
+WHERE ObsTime > (SELECT MAX(ObsTime) FROM old_data)
+{% endif %}
