5923b56485bb0db93cb57d51ad7e94c1687b3917
david27941 <davidho.prime@gmail.com>
2025-01-09 07:18:50 +0000

    test

--- dags/cwa_weather_stream_v2.py
+++ dags/cwa_weather_stream_v2.py
@@ -138,8 +138,8 @@ with DAG(
         },
         headers={"Content-Type": "application/json"},
         log_response=True,
-        # s3_bucket=f"s3://{s3_bucket_name}",
-        s3_key=f"s3://{s3_bucket_name}/weather_record/weather_report_10min-{{ execution_date }}_v2.json",
+        s3_bucket=f"{s3_bucket_name}",
+        s3_key=f"weather_record/weather_report_10min-{{ execution_date }}_v2.json",
         aws_conn_id="aws_s3_conn",
     )
 
