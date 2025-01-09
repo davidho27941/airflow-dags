63bbbc01c9be034f9b521e3e877a988b22e104de
david27941 <davidho.prime@gmail.com>
2025-01-09 07:09:24 +0000

    test

--- dags/cwa_weather_stream_v2.py
+++ dags/cwa_weather_stream_v2.py
@@ -66,42 +66,42 @@ from pathlib import Path
 
 
     
-# @task(task_id='upload_s3', trigger_rule=TriggerRule.ONE_SUCCESS, retries=3, retry_delay=timedelta(minutes=1))
-# def upload_s3(ti, **context):
-#     s3_id = Variable.get('s3-side-project-id')
-#     s3_key = Variable.get('s3-side-project-key')
-#     s3_bucket_name = Variable.get('s3-dev-bucket-name')
-#     s3_region_name = Variable.get('s3-default-region')
+@task(task_id='upload_s3_test', trigger_rule=TriggerRule.ONE_SUCCESS, retries=3, retry_delay=timedelta(minutes=1))
+def upload_s3(ti, **context):
+    s3_id = Variable.get('s3-side-project-id')
+    s3_key = Variable.get('s3-side-project-key')
+    s3_bucket_name = Variable.get('s3-dev-bucket-name')
+    s3_region_name = Variable.get('s3-default-region')
     
-#     data = ti.xcom_pull(task_ids='ping_cwa_api_task')
-#     print(data)
-#     data = json.loads(data)
+    data = ti.xcom_pull(task_ids='ping_cwa_api_task')
+    print(data)
+    data = json.loads(data)
     
-#     s3 = boto3.client(
-#         's3',
-#         region_name=s3_region_name,
-#         aws_access_key_id=s3_id,
-#         aws_secret_access_key=s3_key,
-#     )
+    s3 = boto3.client(
+        's3',
+        region_name=s3_region_name,
+        aws_access_key_id=s3_id,
+        aws_secret_access_key=s3_key,
+    )
     
-#     try:
-#         # Follow the solution provided by: https://stackoverflow.com/questions/46844263/writing-json-to-file-in-s3-bucket
-#         # Alternative method: https://repost.aws/questions/QUemVDeKUTRm-KL7DjjHFtSA/uploading-a-file-to-s3-using-python-boto3-and-codepipeline
-#         timestamp = f"{datetime.now(pytz.timezone('Asia/Taipei')):%Y-%m-%d_%H_%M}"
-#         timestamp_to_pass = f"{datetime.now(pytz.timezone('Asia/Taipei')):%Y-%m-%d %H:%M}"
+    try:
+        # Follow the solution provided by: https://stackoverflow.com/questions/46844263/writing-json-to-file-in-s3-bucket
+        # Alternative method: https://repost.aws/questions/QUemVDeKUTRm-KL7DjjHFtSA/uploading-a-file-to-s3-using-python-boto3-and-codepipeline
+        timestamp = f"{datetime.now(pytz.timezone('Asia/Taipei')):%Y-%m-%d_%H_%M}"
+        timestamp_to_pass = f"{datetime.now(pytz.timezone('Asia/Taipei')):%Y-%m-%d %H:%M}"
         
-#         file_key = f'weather_record/weather_report_10min-{timestamp}.json'
-#         s3.put_object(
-#             Body=json.dumps(data),
-#             Bucket=s3_bucket_name,
-#             Key=file_key
-#         )
-#         info = (file_key, timestamp_to_pass)
-#         return info
+        file_key = f'weather_record/weather_report_10min-{timestamp}_v2_alt.json'
+        s3.put_object(
+            Body=json.dumps(data),
+            Bucket=s3_bucket_name,
+            Key=file_key
+        )
+        info = (file_key, timestamp_to_pass)
+        return info
         
-#     except ClientError as e:
-#         print(e)
-#         raise e
+    except ClientError as e:
+        print(e)
+        raise e
 
 with DAG(
     dag_id='cwa_weather_stream_v_2_0_0',
@@ -112,6 +112,20 @@ with DAG(
     
     token = Variable.get('cwa_auth_token')
     s3_bucket_name = Variable.get('s3-dev-bucket-name')
+    
+    ping_task = HttpOperator(
+        task_id='ping_cwa_api_task',
+        http_conn_id='cwa_real_time_api',
+        endpoint="/api/v1/rest/datastore/O-A0003-001",
+        method='GET',
+        data={
+            'Authorization': f'{token}',
+            'format': "JSON",
+        },
+        headers={"Content-Type": "application/json"},
+        log_response=True,
+    )
+
     
     get_recent_weather_task = HttpToS3Operator(
         task_id='get_weather_to_s3',
@@ -129,20 +143,8 @@ with DAG(
         aws_conn_id="aws_s3_conn",
     )
 
-    get_recent_weather_task
+    [ping_task, get_recent_weather_task] >> upload_s3()
 
-    # ping_task = HttpOperator(
-    #     task_id='ping_cwa_api_task',
-    #     http_conn_id='cwa_real_time_api',
-    #     endpoint="/api/v1/rest/datastore/O-A0003-001",
-    #     method='GET',
-    #     data={
-    #         'Authorization': f'{token}',
-    #         'format': "JSON",
-    #     },
-    #     headers={"Content-Type": "application/json"},
-    #     log_response=True,
-    # )
 
     # check_bucket_existence_task = check_bucket_existence()
     # create_bucket_task = create_bucket()
